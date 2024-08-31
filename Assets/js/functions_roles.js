690c447534266c0af1b142745304c51cf82a48af
Jose Alejandro <josealejandroortegamora@gmail.com>
2024-08-31 17:24:00 -0500

    mostrar roles en datatables

---
+++ Assets/js/functions_roles.js
@@ -0,0 +1,34 @@
+var tableRoles;
+
+document.addEventListener('DOMContentLoaded', function(){
+ 
+    tableRoles = $('#tableRoles').dataTable({
+        "aProcessing":true,
+        "aServerSide":true,
+        "language": { 
+            "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/es-ES.json"
+        },
+        "ajax":{
+            "url": " "+base_url+"/Roles/getRoles",
+            "dataSrc":""
+        },
+        "columns":[
+            {"data": "idrol"},
+            {"data": "nombrerol"},
+            {"data": "descripcion"},
+            {"data": "status"},
+        ],
+        "resonsieve":"true",
+        "bDestroy": true,
+        "iDisplayLength": 10,
+        "order":[[0,"desc"]]
+
+    });
+    
+});
+
+$('#tableRoles').DataTable();
+
+function openModal(){
+    $('#modalFormRol').modal('show');
+}
---
+++ Controllers/Roles.php
@@ -0,0 +1,27 @@
+<?php
+
+class Roles extends Controllers{
+    public function __construct()
+    {
+    parent::__construct();
+    }
+
+
+   public function roles(){
+    $data['page_id'] = 3;
+    $data['page_tag'] = "Roles Usuario";
+    $data['page_title'] = "Roles Usuario <small> Tienda Virtual JAO</small>";
+    $data['page_name'] = "rol_usuario";
+   $this->views->getView($this, "roles", $data);
+   }
+
+   public function getRoles(){
+    $arrData = $this->model->selectRoles();
+
+    echo json_encode($arrData,JSON_UNESCAPED_UNICODE);
+    die();
+   }
+
+}
+
+?>
